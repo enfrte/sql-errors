5e53d3f94b9aef0655e8455ba316435c97e9b7b0
enfrte <enfrte@gmail.com>
2021-07-24 00:46:54 +0300

    Added method

--- SQLErrors.php
+++ SQLErrors.php
@@ -3,5 +3,15 @@
 class SQLErrors {
 	const ERROR_CODES = [
 		1062 => DUPLICATE_ENTRY,
+		// add more error codes 
 	];
+
+	public static function getErrorFromErrorCode($error_code){
+		if (array_key_exists($error_code, self::ERROR_CODES)) {
+			return '<br>Error: ' . self::ERROR_CODES[$error_code];
+		}
+		else {
+			echo '<br>Unknown Error.';
+		}
+	}
 }
--- index.php
+++ index.php
@@ -7,13 +7,7 @@ include_once 'db.php';
 try {
 	$sql = "INSERT INTO users (hetu) VALUES ('444D')";
 	$conn->exec($sql); // use exec() because no results are returned
-	echo "New record created successfully";
+	echo "<br>New record created successfully.";
 } catch (PDOException $e) {
-	$SQL_ERRORS = new SQLErrors();
-	if (array_key_exists($e->errorInfo[1], $SQL_ERRORS::ERROR_CODES)) {
-		echo '<br>Error: ' . $SQL_ERRORS::ERROR_CODES[$e->errorInfo[1]];
-	}
-	else {
-		echo '<br>Unknown Error.';
-	}
+	echo SQLErrors::getErrorFromErrorCode($e->errorInfo[1]);
 }
